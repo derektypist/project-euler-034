4d86525d3cd27598b9c09cd204a7313074bd19d6
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-07-29 09:16:39 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const FACTORIALS = [1,1,2,6,24,120,720,5040,40320,362880];
 // Function to Show Solution
 function showSolution() {
     // Set Up Variable
-    let txt = `Solution is ${digitFactorial()} <br>`;
+    let txt = `Solution is ${digitFactorial().sum} <br>`;
     // Display Solution in the Browser
     document.getElementById("solution").innerHTML = txt;
 }
